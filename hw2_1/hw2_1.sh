6a81bfb78afd46f59b15094f3a3e6802949c60ce
Wei-Chun Hung <wchung0616@bsd1.cs.nctu.edu.tw>
2018-10-21 13:48:57 +0800

    dir

---
+++ hw2_1/hw2_1.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+ls -ARl | grep "^[-d]" | awk '/-/{print $5,$9}/d/{print "dir"}' | sort -n -r | awk 'BEGIN{dir_num=0;total=0}NR<=5{print NR": "$1,$2}$1=="dir"{++dir_num}$1!="dir"{total+=$1}END{print "Dir num: "dir_num"\nFile num: "NR-dir_num"\nTotal: "total}'
