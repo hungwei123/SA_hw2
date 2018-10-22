1a6f672a800cdcbcef978f30368a597437e62f48
Wei-Chun Hung <wchung0616@bsd1.cs.nctu.edu.tw>
2018-10-22 16:36:17 +0800

    done

--- hw2_2/json_download.sh
+++ hw2_2/json_download.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-curl 'https://timetable.nctu.edu.tw/?r=main/get_cos_list' --data 'm_acy=107&m_sem=1&m_degree=3&m_dep_id=17&m_group=**&m_grade=**&m_class=**&m_option=**&m_crs name=**&m_teaname=**&m_cos_id=**&m_cos_code=**&m_crstime=**&m_crsoutline=**&m_costype=**'
+curl 'https://timetable.nctu.edu.tw/?r=main/get_cos_list' --data 'm_acy=107&m_sem=1&m_degree=3&m_dep_id=17&m_group=**&m_grade=**&m_class=**&m_option=**&m_crs name=**&m_teaname=**&m_cos_id=**&m_cos_code=**&m_crstime=**&m_crsoutline=**&m_costype=**' > json.txt
+
+sh deal_with_json.sh
+
